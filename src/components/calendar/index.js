1999c6663e9deb9a7a1d282c609766cc79a29ff7
Manu Krishnan <manu.krishnan@tarams.com>
2018-02-21 13:20:22 +0530

    calendar logic implementation

--- src/components/calendar/index.js
+++ src/components/calendar/index.js
@@ -1,11 +1,80 @@
 import React from 'react';
 import './style.css';
+import Week from './week'
 class Calendar extends React.Component {
     constructor(props) {
-        super(props)
+        super(props);
+        const date = new Date();
+        const today = date.getDay();
+        const month = date.getMonth();
+        const year = date.getFullYear();
+        this.state = {
+            dayList: ['Sun', 'Mon', 'Tue',
+                'Wed', 'Thu', 'Fri', 'Sat'],
+            startDay: 'Sun',
+            monthList: ['Jan', 'Feb', 'Mar',
+                'Apr', 'May', 'Jun', 'Jul',
+                'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+            today, month, year
+        }
+        this.renderWeekHead = this.renderWeekHead.bind(this);
+        this.renderWeeks = this.renderWeeks.bind(this);
+        this.getDaysInMonth = this.getDaysInMonth.bind(this);
     }
+    getDaysInMonth() {
+        const { month, year } = this.state;
+        var date = new Date(year, month, 1);
+        var days = [];
+        while (date.getMonth() === month) {
+            days.push(new Date(date));
+            date.setDate(date.getDate() + 1);
+        }
+        return days;
+    }
+
+
     render() {
-        return (<div className='cal'>calendar</div>);
+        console.log(this.getDaysInMonth(this.state.month, this.state.year))
+        return (<div className='cal'>
+            <table>
+                <tbody>
+                    {this.renderWeekHead()}
+                    {this.renderWeeks()}
+                </tbody>
+            </table>
+
+        </div>);
+    }
+    renderWeekHead() {
+        return <tr>{this.state.dayList.map((day) => <th key={day}>{day}</th>)}</tr>;
+    }
+    renderWeeks() {
+        const { month, year } = this.state;
+        var date = new Date(year, month, 1);
+        var days = [];
+        var rows = [];
+        var week = [];
+        while (date.getMonth() === month) {
+            var curr = new Date(date);
+            days.push(curr);
+            week = this.pushDay(curr, week)
+            if (week.length === 7) {
+                rows.push(<Week days={week}/>);
+                week = [];
+            }
+            date.setDate(date.getDate() + 1);
+        }
+        return rows;
+
+    }
+
+    pushDay(day, week) {
+        week = week || [];
+        week[day.getDay()] = day;
+        return week;
+    }
+    pushEmpty(){
+        
     }
 }
 export default Calendar;
---
+++ src/components/calendar/week.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './style.css';
+class Week extends React.Component {
+    // constructor(props) {
+    //     super(props);
+    // }
+    render() {
+        this.props.days.map((day,ind)=>console.log(day,ind));
+        return <tr>
+            {this.props.days &&
+                this.props.days.map(
+                    (day) => day ? (<td key={day.getDate()}>{day.getDate()}</td>) : <td> - </td>
+                )
+            }
+        </tr>
+    }
+    render2() {
+        return <tr>
+            {this.props.days &&
+                this.props.days.map(
+                    (day) => day ? (<td>{day.getDate()}</td>) : <td> - </td>
+                )
+            }
+        </tr>
+    }
+}
+export default Week;
